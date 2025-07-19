71fa78e50d5daa65f0a8ce152edae261195d6023
Prakhar Srivastava <prakharbksc9@gmail.com>
2025-07-19 13:01:45 +0530

    added resume downloaded botton with backend

--- script.js
+++ script.js
@@ -26,3 +26,22 @@ function showPopup(bool) {
     document.getElementById('popup').style.visibility = 'hidden'
   }
 }
+
+function downloadResume() {
+  // Replace this URL with your actual resume link (Google Drive, Dropbox, GitHub, etc.)
+  const resumeUrl = 'https://drive.google.com/file/d/1egUGwRPE6XtbLNA-v4fl1E4hrx9l0Nyp/view?usp=drive_link';
+  
+  // Alternative: Direct link to resume file
+  // const resumeUrl = 'https://your-website.com/path/to/resume.pdf';
+  
+  // Create temporary link and trigger download
+  const link = document.createElement('a');
+  link.href = resumeUrl;
+  link.download = 'Prakhar_Srivastava_Resume.pdf';
+  link.target = '_blank';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  
+  console.log('Resume download initiated');
+}
